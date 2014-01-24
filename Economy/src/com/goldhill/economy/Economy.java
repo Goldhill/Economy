aa5171cc0b46d25e545818861659115293ae6bb3
Hypnoatic <hypnoatic@aol.com>
2014-01-24 17:51:13 -0500

    Initial release

---
+++ Economy/src/com/goldhill/economy/Economy.java
@@ -0,0 +1,18 @@
+package com.goldhill.economy;
+
+import java.util.logging.Logger;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Economy extends JavaPlugin {//bukkit shit to make it a legit plugin (define main class)
+
+	public void onEnable() {
+		//nothing to log here bukkit auto generated enable and disable logs
+	}
+	
+	public void onDisable() {
+		//nothing to log here
+	}
+
+
+}
